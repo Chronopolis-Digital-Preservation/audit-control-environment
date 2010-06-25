4c53f50c594c1ef1b2fa767beaad232ef91d7e3d
toaster <toaster@f1b3a171-7291-4a19-a512-95ad0ad9394a>
2010-06-25 15:03:08 +0000

    1.5 release tag
    git-svn-id: https://subversion.umiacs.umd.edu/ace@13 f1b3a171-7291-4a19-a512-95ad0ad9394a

---
+++ tags/1.5/trunk/ace-am/src/java/edu/umiacs/ace/util/PersistUtil.java
@@ -0,0 +1,90 @@
+/*
+ * Copyright (c) 2007-2010, University of Maryland
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification, are permitted provided
+ * that the following conditions are met:
+ *
+ * Redistributions of source code must retain the above copyright notice, this list of conditions
+ * and the following disclaimer.
+ *
+ * Redistributions in binary form must reproduce the above copyright notice, this list of conditions
+ * and the following disclaimer in the documentation and/or other materials provided with the
+ * distribution.
+ *
+ * Neither the name of the University of Maryland nor the names of its contributors may be used to
+ * endorse or promote products derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED
+ * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
+ * PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+ * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR
+ * TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+ * ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ * ACE Components were written in the ADAPT Project at the University of
+ * Maryland Institute for Advanced Computer Study.
+ */
+// $Id$
+
+package edu.umiacs.ace.util;
+
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import javax.sql.DataSource;
+
+/**
+ *
+ * @author toaster
+ */
+public class PersistUtil {
+
+    private static EntityManagerFactory emf = Persistence.createEntityManagerFactory(
+            "ace-amPU");
+    private static DataSource ds = null;
+
+    public static void closeFactory() {
+        emf.close();
+        emf = null;
+    }
+
+    public static DataSource getDataSource() {
+        if ( ds == null ) {
+            try {
+                Context ctx = new InitialContext();
+                ds = (DataSource) ctx.lookup("java:comp/env/jdbc/aceamdb");
+            } catch ( NamingException e ) {
+                throw new RuntimeException(e);
+            }
+        }
+        return ds;
+    }
+
+    public static void persist( Object object ) {
+        EntityManager em = getEntityManager();
+        try {
+            em.getTransaction().begin();
+            em.persist(object);
+            em.getTransaction().commit();
+        } catch ( Exception e ) {
+            java.util.logging.Logger.getLogger(PersistUtil.class.getName()).log(
+                    java.util.logging.Level.SEVERE, "exception caught", e);
+            em.getTransaction().rollback();
+        } finally {
+            em.close();
+        }
+    }
+
+    public static EntityManager getEntityManager() {
+        if ( emf == null ) {
+            emf = Persistence.createEntityManagerFactory("ace-amPU");
+        }
+        return emf.createEntityManager();
+    }
+}
---
+++ tags/1.5/trunk/ace-am/web/collectionfinish.jsp
@@ -0,0 +1,46 @@
+<%-- 
+Document   : resourcefinish
+Created on : Nov 2, 2007, 2:00:03 PM
+Author     : toaster
+--%>
+
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN"
+"http://www.w3.org/TR/html4/loose.dtd">
+
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>Collection Updated</title>
+        <link rel="stylesheet" type="text/css" href="style.css" />
+    </head>
+    <body>
+        <jsp:include page="header.jsp"/>
+        <div class="standardBody">
+            <h3>Collection Modified/Saved</h3>
+            <table >
+                <thead>
+                    <tr>
+                        <th></th>
+                        <th></th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>Name</td>
+                        <td>${collection.name}</td>
+                    </tr>
+                    <tr>
+                        <td>Source Directory</td>
+                        <td>${collection.directory}</td>
+                    </tr>
+                    <tr>
+                        <td><a href="Status">Back</a></td>
+                    </tr>
+                </tbody>
+            </table>
+            
+        </div>
+        <jsp:include page="footer.jsp"/>
+    </body>
+</html>
