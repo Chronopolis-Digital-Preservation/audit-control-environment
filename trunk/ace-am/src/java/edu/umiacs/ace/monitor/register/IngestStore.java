7690534b30758954b211491196c76701513b6941
toaster <toaster@f1b3a171-7291-4a19-a512-95ad0ad9394a>
2011-01-12 19:32:16 +0000

    maven start on ace-am
    git-svn-id: https://subversion.umiacs.umd.edu/ace@45 f1b3a171-7291-4a19-a512-95ad0ad9394a

---
+++ trunk/ace-am/src/java/edu/umiacs/ace/monitor/register/IngestStore.java
@@ -0,0 +1,32 @@
+package edu.umiacs.ace.monitor.register;
+
+import edu.umiacs.ace.monitor.core.Collection;
+import edu.umiacs.ace.monitor.core.MonitoredItem;
+import edu.umiacs.ace.util.EntityManagerServlet;
+import java.io.IOException;
+import javax.persistence.EntityManager;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.apache.tomcat.util.http.fileupload.FileUploadBase;
+
+/**
+ *
+ * @author toaster
+ */
+public class IngestStore extends EntityManagerServlet {
+    
+    @Override
+    protected void processRequest( HttpServletRequest request, HttpServletResponse response,
+            EntityManager em ) throws ServletException, IOException {
+
+        Collection coll = getCollection(request, em);
+        MonitoredItem item = getItem(request, em);
+
+        if (item != null && !item.isDirectory())
+            throw new ServletException("Selected item is not a directory " + item.getPath());
+
+//        request.
+    }
+   
+}
