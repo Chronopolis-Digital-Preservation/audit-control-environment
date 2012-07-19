17b138f2539996a79e785d3eb014c746c0c90a1a
shake <shake@f1b3a171-7291-4a19-a512-95ad0ad9394a>
2012-07-19 19:48:09 +0000

    utf8 support
    git-svn-id: https://subversion.umiacs.umd.edu/ace/trunk@150 f1b3a171-7291-4a19-a512-95ad0ad9394a

--- ace-am/src/main/sql/1.6-1.7.updates.sql
+++ ace-am/src/main/sql/1.6-1.7.updates.sql
@@ -5,14 +5,14 @@ CREATE TABLE `settings` (
   `COLLECTION_ID` bigint(20) NOT NULL,
   PRIMARY KEY  (`ID`),
   UNIQUE idx_coll_setting (`COLLECTION_ID`,`ATTR`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
 
-ALTER TABLE monitored_item CHARACTER SET utf8 COLLATE  utf8_general_ci;
-ALTER TABLE monitored_tem MODIFY `PARENTPATH` varchar(512) CHARACTER SET utf8 COLLATE utf8_general_ci;
-ALTER TABLE monitored_tem MODIFY `PATH` varchar(512) CHARACTER SET utf8 COLLATE utf8_general_ci;
-ALTER TABLE monitored_tem MODIFY `FILEDIGEST` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci;
-ALTER TABLE monitored_tem MODIFY `STATE` char(1) CHARACTER SET utf8 COLLATE utf8_general_ci;
-ALTER TABLE logevent CHARACTER SET utf8 COLLATE  utf8_general_ci;
-ALTER TABLE logevent MODIFY `DESCRIPTION` text CHARACTER SET utf8 COLLATE utf8_general_ci;
-ALTER TABLE logevent MODIFY `PATH` text CHARACTER SET utf8 COLLATE utf8_general_ci;
+ALTER TABLE monitored_item CHARACTER SET utf8;
+ALTER TABLE monitored_item MODIFY `PARENTPATH` varchar(512) CHARACTER SET utf8;
+ALTER TABLE monitored_item MODIFY `PATH` varchar(512) CHARACTER SET utf8;
+ALTER TABLE monitored_item MODIFY `FILEDIGEST` varchar(255) CHARACTER SET;
+ALTER TABLE monitored_item MODIFY `STATE` char(1) CHARACTER SET;
+ALTER TABLE logevent CHARACTER SET;
+ALTER TABLE logevent MODIFY `DESCRIPTION` text CHARACTER SET utf8;
+ALTER TABLE logevent MODIFY `PATH` text CHARACTER SET utf8;
