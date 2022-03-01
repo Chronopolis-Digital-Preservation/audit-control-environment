74b9365e0bac77740f52d27dc02d7693ef8a9736
Thor Brickman <tnbrickman@ucsd.edu>
2022-03-01 15:02:30 -0800

    Java and Tomcat working.

---
+++ docker/devenv/devup.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+echo "############################################ Bringing up dev environment."
+docker-compose --project-name ace up -d
+echo "############################################ Build dev running."
